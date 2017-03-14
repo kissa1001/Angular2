414c19f13fd9517491f709c7126f9cf673fa4df7
kissa1001 <kissa101295@gmail.com>
2017-03-13 22:05:26 -0700

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>hello</h1>
+    <fa-databinding></fa-databinding>
+  `
 })
 export class AppComponent {
   title = 'Hello World!';
