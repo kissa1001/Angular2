ec0cac8ee02ed31fd8c7c5ae1a7c74e8fd0c5a36
kissa1001 <kissa101295@gmail.com>
2017-03-14 00:06:56 -0700

    property and event binding

--- src/app/Databinding/databinding.component.ts
+++ src/app/Databinding/databinding.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fa-databinding',
   templateUrl: 'databinding.component.html',
-  styles: []
+  styleUrls: ['databinding.component.css']
 })
 export class DatabindingComponent {
   stringInterpolation = "This is string interpolation!";
   numberInterpolation = 2;
+
+  onTest() {
+    return true;
+  }
 }
